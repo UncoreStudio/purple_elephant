e7c2f33377724986ea74e0fcbc8113da6c6ffbc4
Azonesbz <vincent.avez22@gmail.com>
2024-04-07 09:14:29 +0200

    update programme

--- rpc-client/src/index.js
+++ rpc-client/src/index.js
@@ -1,34 +1,45 @@
-import web3, { Connection, Keypair, Transaction, TransactionInstruction } from '@solana/web3.js';
-import fs from "node:fs";
-let connection = new Connection("https://api.devnet.solana.com", "confirmed");
+import web3, { sendAndConfirmTransaction } from "@solana/web3.js"
+import fs from "node:fs"
+import path from "path"
 
-const triggerKeyPair = Keypair.generate();
+// Créer un client RPC pour interagir avec le réseau Solana
+const rpcUrl = 'http://127.0.0.1:8899'; // Remplacez par l'URL du nœud RPC Solana
+const rpc = new web3.Connection(rpcUrl);
 
-const transaction = new Transaction({
-    feePayer: triggerKeyPair.publicKey,
-})
+// Remplacer ces valeurs avec les clés publiques des comptes impliqués
+const programId = new web3.PublicKey('GYLrDmuv4bmF7p3kRJrFYAxpx6zaPHcjwJULuTEERbtP'); // ID de votre programme Solana
+const liquidityPoolAccountPubkey = web3.Keypair.generate(); // ID de la pool de liquidité
 
+const keypairConfig = JSON.parse(fs.readFileSync(path.join(process.cwd(), '../.config/solana/id.json'), 'utf8'));
+const keypair = web3.Keypair.fromSecretKey(
+    Uint8Array.from(keypairConfig)
+);
 
-const makeTransaction = async (programId, keyPairFile) => {
-    // Create a array of keys
-    const borrowerKeyPair = Keypair.fromSecretKey(new Uint8Array(JSON.parse(fs.readFileSync(keyPairFile))))
+const userAccountPubkey = new web3.PublicKey(keypair.publicKey);
 
-    transaction.add(
-        new TransactionInstruction({
-            keys: [{ pubkey: borrowerKeyPair.publicKey, isSigner: true, isWritable: true }],
-            programId,
-            data: Buffer.alloc(0), // All instructions are empty
-        }),
-        
-    )
-    await web3.sendAndConfirmTransaction(connection, transaction, [
-        Keypair.generate(),
-        keyPairFile,
-      ]);
-    console.log('Transaction confirmed');
-}
+// Créer une instruction pour déposer dans la pool de liquidité
 
-const programId = new Keypair('GYLrDmuv4bmF7p3kRJrFYAxpx6zaPHcjwJULuTEERbtP');
+const instruction = new web3.TransactionInstruction({
+    programId,
+    keys: [
+        { pubkey: liquidityPoolAccountPubkey.publicKey, isSigner: false, isWritable: true },
+        { pubkey: userAccountPubkey, isSigner: true, isWritable: true },
+    ],
+    data: instructionData,
+});
 
-makeTransaction(programId, '/home/azones/.config/solana/id.json');
+// Créer une transaction pour envoyer l'instruction
+const transaction = new web3.Transaction().add(instruction);
 
+// Signer la transaction avec la clé privée du compte payeur
+const result = await rpc.getLatestBlockhash();
+
+transaction.recentBlockhash = result.blockhash;
+const from = web3.Keypair.fromSecretKey(
+    Buffer.from(fs.readFileSync(path.join(process.cwd(), '../.config/solana/id.json'), 'utf-8'))
+)
+sendAndConfirmTransaction(rpc, transaction, [from]).then(result => {
+    console.log('Transaction envoyée:', result);
+}).catch(error => {
+    console.error('Erreur lors de l\'envoi de la transaction:', error);
+});
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use std::io::Write;
-use arrayref::array_ref;
 use solana_program::{
     account_info::{next_account_info, AccountInfo}, 
     entrypoint::ProgramResult,
@@ -16,24 +14,45 @@ pub struct LiquidityPool {
 }
 
 impl LiquidityPool {
-    pub fn pack(pool: Self, dst: &mut [u8]) -> Result<(), ProgramError> {
-        let mut writer = std::io::Cursor::new(dst);
-        writer.write_all(&pool.authority.to_bytes())?;
-        writer.write_all(&pool.total_liquidity.to_le_bytes())?;
-        writer.write_all(&pool.available_liquidity.to_le_bytes())?;
-        writer.write_all(&[if pool.is_initialized { 1 } else { 0 }])?;
+    pub fn pack(pool: &LiquidityPool, dst: &mut [u8]) -> Result<(), ProgramError> {
+        if dst.len() < 49 {
+            return Err(ProgramError::InvalidArgument);
+        }
+
+        dst[..32].copy_from_slice(&pool.authority.to_bytes());
+
+        dst[32..40].copy_from_slice(&pool.total_liquidity.to_le_bytes());
+
+        dst[40..48].copy_from_slice(&pool.available_liquidity.to_le_bytes());
+
+        dst[48] = 1;
+
         Ok(())
     }
 
-    // Fonction pour désérialiser les octets en une structure de pool de liquidité
-    pub fn unpack(src: &[u8]) -> Result<Self, ProgramError> {
-        let _cursor = std::io::Cursor::new(src);
-        let authority = Pubkey::new_from_array(*array_ref![src, 0, 32]);
-        let total_liquidity = u64::from_le_bytes(*array_ref![src, 32, 8]);
-        let available_liquidity = u64::from_le_bytes(*array_ref![src, 40, 8]);
-        let is_initialized_byte = array_ref![src, 48, 1][0]; // Obtenez le premier octet à partir de l'index 48
-        let is_initialized = is_initialized_byte != 0; // Convertissez l'octet en booléen
-        Ok(Self {
+    pub fn unpack(src: &[u8]) -> Result<LiquidityPool, ProgramError> {
+        if src.len() < 49 {
+            // Si les données sont trop courtes, cela signifie que la pool n'existe pas
+            // Créez une nouvelle instance de LiquidityPool avec les valeurs par défaut
+            return Ok(LiquidityPool {
+                authority: Pubkey::default(), // Utilisez la clé par défaut
+                total_liquidity: 0,            // Aucune liquidité totale
+                available_liquidity: 0,        // Aucune liquidité disponible
+                is_initialized: false,         // La pool n'est pas initialisée
+            });
+        }
+        let authority_bytes: [u8; 32] = src[..32].try_into().map_err(|_| ProgramError::InvalidAccountData)?;
+        let authority = Pubkey::from(authority_bytes);
+
+        let total_liquidity_bytes: [u8; 8] = src[32..40].try_into().map_err(|_| ProgramError::InvalidAccountData)?;
+        let total_liquidity = u64::from_le_bytes(total_liquidity_bytes);
+
+        let available_liquidity_bytes: [u8; 8] = src[40..48].try_into().map_err(|_| ProgramError::InvalidAccountData)?;
+        let available_liquidity = u64::from_le_bytes(available_liquidity_bytes);
+
+        let is_initialized = src[48] != 0;
+
+        Ok(LiquidityPool {
             authority,
             total_liquidity,
             available_liquidity,
@@ -43,7 +62,27 @@ impl LiquidityPool {
 }
 
 pub fn deposit_to_liquidity_pool(
-    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    _amount: u64,
+) -> ProgramResult {
+    // Vérifier les comptes
+    let account_iter = &mut accounts.iter();
+    let liquidity_pool_account = next_account_info(account_iter)?;
+    let user_account = next_account_info(account_iter)?;
+
+    let mut liquidity_pool_data_refcell = liquidity_pool_account.data.borrow_mut();
+    let liquidity_pool_data_slice = &mut *liquidity_pool_data_refcell;
+    let mut liquidity_pool_data = LiquidityPool::unpack(&liquidity_pool_data_slice)?;
+    liquidity_pool_data.total_liquidity += 11;
+    liquidity_pool_data.available_liquidity += 11;
+    LiquidityPool::pack(&liquidity_pool_data, liquidity_pool_data_slice)?;
+
+    transfer(user_account.key, liquidity_pool_account.key, 11);
+
+    Ok(())
+}
+
+pub fn withdraw_from_liquidity_pool(
     accounts: &[AccountInfo],
     amount: u64,
 ) -> ProgramResult {
@@ -53,16 +92,15 @@ pub fn deposit_to_liquidity_pool(
     let user_account = next_account_info(account_iter)?;
 
     // Vérifier que le programme est le propriétaire de la pool de liquidité
-    if *liquidity_pool_account.owner != *program_id {
-        return Err(ProgramError::IncorrectProgramId);
-    }
 
-    let mut liquidity_pool_data = LiquidityPool::unpack(&liquidity_pool_account.data.borrow())?;
-    liquidity_pool_data.total_liquidity += amount;
-    liquidity_pool_data.available_liquidity += amount;
-    LiquidityPool::pack(liquidity_pool_data, &mut liquidity_pool_account.data.borrow_mut())?;
+    let liquidity_pool_data_refcell = liquidity_pool_account.data.borrow();
+    let liquidity_pool_data_slice = &*liquidity_pool_data_refcell;
+    let mut liquidity_pool_data = LiquidityPool::unpack(&liquidity_pool_data_slice)?;
+    liquidity_pool_data.total_liquidity -= amount;
+    liquidity_pool_data.available_liquidity -= amount;
+    LiquidityPool::pack(&liquidity_pool_data, &mut liquidity_pool_account.data.borrow_mut())?;
 
-    transfer(user_account.key, liquidity_pool_account.key, amount);
+    transfer(liquidity_pool_account.key, user_account.key, amount);
 
     Ok(())
 }
@@ -70,18 +108,18 @@ pub fn deposit_to_liquidity_pool(
 entrypoint!(process_instruction);
 
 pub fn process_instruction(
-    program_id: &Pubkey,
+    _program_id: &Pubkey,
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
     // Vérifie l'instruction et détermine quelle fonction appeler en fonction de l'instruction_data
     if instruction_data == b"deposit" {
-        // Appel de la fonction deposit_to_liquidity_pool avec un montant spécifique (par exemple, 1000)
-        deposit_to_liquidity_pool(program_id, accounts, 1000)?;
-    } else {
-        // Gérer d'autres instructions
-        return Err(ProgramError::InvalidInstructionData);
+        // Appel de la fonction deposit_to_liquidity_pool avec un montant spécifique (par exemple, 10)
+        deposit_to_liquidity_pool(accounts, 10)?;
+    } 
+    
+    if instruction_data == b"withdraw" {
+        withdraw_from_liquidity_pool(accounts, 10)?;
     }
-
-    Ok(())
+    return Err(ProgramError::InvalidInstructionData);
 }
